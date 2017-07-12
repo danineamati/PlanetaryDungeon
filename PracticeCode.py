d9c2c666382ec0d9932a5a7438b51ac0ca9036b1
Daniel Neamati <danineamati@gmail.com>
2017-07-12 10:59:24 -0400

    Added the print function and added wall generation. Need function for wall generation.

--- PracticeCode.py
+++ PracticeCode.py
@@ -12,6 +12,13 @@ def setBoardLocation(rowcolumn, item_type):
     print "You have set your", str(item_type), "at", str(rowcolumn), str(location)
     return int(location)
 
+def printBoard(Column_identifier, Column_list):
+    print ""
+    #Print the grid
+    print "x", Column_identifier
+    for row in range(len(Column_list)):
+        print row, Column_list[row]
+
 #Set up the lists
 for row in range(rows):
     Column_list.append(["O"] * columns) #O for Open
@@ -19,9 +26,7 @@ for row in range(rows):
 for column in range(columns + 1):
     Column_identifier.append(str(column))
 
-print "x", Column_identifier
-for row in range(len(Column_list)):
-    print row, Column_list[row]
+printBoard(Column_identifier, Column_list)
 
 #Set borders
 #The first and last index of each row needs to be walls
@@ -42,11 +47,7 @@ for column in range(columns + 1):
     final_row.append("X")
 Column_list.append(final_row) #list from 0 to columns
 
-print ""
-#Print the grid
-print "x", Column_identifier
-for row in range(len(Column_list)):
-    print row, Column_list[row]
+printBoard(Column_identifier, Column_list)
 
 #Set Boss Location
 Boss_row = setBoardLocation("row", "Boss")
@@ -54,8 +55,18 @@ Boss_column = setBoardLocation("column", "Boss")
 
 Column_list[Boss_row][Boss_column] = "B"
 
-print ""
-#Print the grid
-print "x", Column_identifier
-for row in range(len(Column_list)):
-    print row, Column_list[row]
+printBoard(Column_identifier, Column_list)
+
+print "Let's set your first wall!"
+#Set Wall Location
+Wall1_row = setBoardLocation("row", "Wall") #Note the 1 (one) not l
+Wall1_column = setBoardLocation("column", "Wall")
+
+if Wall1_row != Boss_row or Wall1_column != Boss_column:
+    Column_list[Wall1_row][Wall1_column] = "W"
+else:
+    print "That spot is already taken!"
+    Wall1_row = setBoardLocation("row", "Wall") #Again, note the 1 (one) not l
+    Wall1_column = setBoardLocation("column", "Wall")
+
+printBoard(Column_identifier, Column_list)
