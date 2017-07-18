492816665d195576ea9747b8d87674ab3ac77b6c
Daniel Neamati <danineamati@gmail.com>
2017-07-17 21:44:54 -0400

    Added preliminary Hero Movement code.

--- Py_3.6-PlanetDungeon_Closed-Alpha_ver_0.1.py
+++ Py_3.6-PlanetDungeon_Closed-Alpha_ver_0.1.py
@@ -9,21 +9,21 @@ Column_identifier = []
 def setBoardLocation(rowcolumn, item_type, rowscolumnsindex):
     locationset = False
     while locationset == False:
-        print "Which", str(rowcolumn), "would you like your", str(item_type), "?"
-        location = int(raw_input())
+        print("Which", str(rowcolumn), "would you like your", str(item_type), "?")
+        location = int(input())
         if location > 0 and location <= rowscolumnsindex:
-            print "You have set your", str(item_type), "at", str(rowcolumn), str(location)
+            print("You have set your", str(item_type), "at", str(rowcolumn), str(location))
             return int(location)
             locationset = True
         else:
-            print "This location is not on the board. Try another location."
+            print("This location is not on the board. Try another location.")
 
 def printBoard(Column_identifier, Column_list):
-    print ""
+    print("")
     #Print the grid
-    print "x", Column_identifier
+    print("x", Column_identifier)
     for row in range(len(Column_list)):
-        print row, Column_list[row]
+        print(row, Column_list[row])
 
 def setWall(Column_list, columns, rows):
     wallset = False
@@ -33,12 +33,12 @@ def setWall(Column_list, columns, rows):
         if Column_list[Wall_row][Wall_column] == "W" or \
            Column_list[Wall_row][Wall_column] == "X" or \
            Column_list[Wall_row][Wall_column] == "S":
-            print "There is already a Wall there! Try another location."
+            print("There is already a Wall there! Try another location.")
         elif Wall_row != Boss_row or Wall_column != Boss_column:
             Column_list[Wall_row][Wall_column] = "W"
             wallset = True
         else:
-            print "That spot is already taken! Try another location" 
+            print("That spot is already taken! Try another location")
 
 #Set up the lists
 for row in range(rows):
@@ -80,11 +80,11 @@ Column_list[Boss_row][Boss_column] = "B"
 
 printBoard(Column_identifier, Column_list)
 
-print "Let's set your first wall!"
+print("Let's set your first wall!")
 #Set Wall Location of first wall
 setWall(Column_list, columns, rows)
 printBoard(Column_identifier, Column_list)
-print "Now try another!"
+print("Now try another!")
 #Set Wall Location of second wall
 setWall(Column_list, columns, rows)
 printBoard(Column_identifier, Column_list)
@@ -120,28 +120,30 @@ def AdjacentLoop(Column_identifier, Column_list, Boss_row, Boss_column):
             AdjacentList[point][3] = True
             #Check if the the adjacent room is valid
             newpoint = [] #initialize or re-initialize
-            #print "newpoint initial", newpoint
+            #print("newpoint initial", newpoint)
             newpoint = range(len(AdjacentList) - 4, \
                              len(AdjacentList)) #Index of the new four items
-            #print "length", len(AdjacentList)
-            #print "newpoint", newpoint
+            #print("length", len(AdjacentList))
+            #print("newpoint", newpoint)
             for new in newpoint:
                 #print "new", new
                 column =  AdjacentList[new][1] #column coordinate
                 row = AdjacentList[new][0] #row coordinate
-                print "row", row, "column", column
+                #print("row", row, "column", column)
                 if Column_list[row][column] == "S": #Start
-                    print "The hero has found the boss!"
-                    print "Hero traverses", str(count), "rooms."
+                    print("The hero has found the boss!")
+                    print("Hero traverses", str(count), "rooms.")
                     bossfound = True
                 elif Column_list[row][column] == "O": #Check entered criteria
                     #print "Next open point:", AdjacentList[new]
                     Column_list[row][column] = str(count)
                     AdjacentList[new][2] = True
                 elif Column_list[row][column] == "X" or Column_list[row][column] == "W": #Check Wall
-                    print "Hit a wall at", AdjacentList[new]
+                    #print("Hit a wall at", AdjacentList[new])
+                    pass
                 elif Column_list[row][column] == "B": #Check Boss
-                    print "Boss location known"
+                    #print("Boss location known")
+                    pass
             for index in range(len(AdjacentList)):
                 if AdjacentList[len(AdjacentList) - 1][2] == False:
                     AdjacentList.pop(len(AdjacentList) - 1) #remove all invalid rooms
@@ -150,7 +152,7 @@ def AdjacentLoop(Column_identifier, Column_list, Boss_row, Boss_column):
         reversed(AdjacentList)
         seen = 0
         for index in range(len(AdjacentList)):
-            print AdjacentList[seen]
+            #print(AdjacentList[seen])
             if AdjacentList[seen][2] == False:
                 AdjacentList.pop(len(AdjacentList) - seen) #remove all invalid
                 #print "popped"
@@ -158,13 +160,75 @@ def AdjacentLoop(Column_identifier, Column_list, Boss_row, Boss_column):
                 seen += 1
                 #print "up one:", seen
         reversed(AdjacentList)
-        print len(AdjacentList)
+        #print(len(AdjacentList))
         #print AdjacentList
-        print "End loop", count
+        print("End loop", count)
         count += 1
         num += 1
-    print "The hero has found the boss!"
-    print "Hero traverses", str(count - 1), "rooms."
+    print("The hero has found the boss!")
+    print("Hero will traverse", str(count - 1), "rooms.")
         
 AdjacentLoop(Column_identifier, Column_list, Boss_row, Boss_column)
-print "Game complete!"
+print("Hero has discovered its intended path!")
+
+#Start Hero movement
+def boss_reached(): #This prints the "Boss Reached" interface
+    print("The boss has been reached!")
+
+def conditional_adjacent(Column_identifier, Column_list, Checking_row, Checking_column):
+    Bossreached = False
+    if Column_list[Checking_row][Checking_column] == "B":
+        boss_reached()
+        return ["Boss", [Checking_row], [Checking_column]]
+        BossLocation = [[Checking_row - 1], [Checking_column]]
+    elif Column_list[Checking_row - 1][Checking_column] == "W" \
+         or Column_list[Checking_row - 1][Checking_column] == "X" \
+         or Column_list[Checking_row - 1][Checking_column] == "O":
+        pass
+    else:
+        return ["Tile", [Checking_row], [Checking_column]]
+
+def check_adjacent(Column_identifier, Column_list, Current_row, Current_column): #This returns the coordinate of the next lowest number
+    TileValue = []
+    print("Current_row", Current_row)
+    print("Current_column", Current_column)
+    print("Column_list value", Column_list[Current_row][Current_column])
+    Bossreached = False
+    #Check Above
+    TileValue.append(conditional_adjacent(Column_identifier, Column_list, Current_row - 1, Current_column))
+    #Check Right
+    TileValue.append(conditional_adjacent(Column_identifier, Column_list, Current_row, Current_column + 1))
+    #Check Below
+    TileValue.append(conditional_adjacent(Column_identifier, Column_list, Current_row + 1, Current_column))
+    #Check Left
+    TileValue.append(conditional_adjacent(Column_identifier, Column_list, Current_row, Current_column - 1))
+    TileValue.sort()
+    print(TileValue)
+    print(TileValue[0][0], "First Value")
+    print(TileValue[0][1], "Row value")
+    print(TileValue[0][2], "Column value")
+    for item in range(len(TileValue)):
+        if TileValue[0] == "Boss":
+            return ["Boss", TileValue[item][0], BossLocation[item][1]]
+    return ["next", TileValue[0][1], TileValue[0][2]]
+
+def hero_location(Column_identifier, Column_list): #This updates hero location
+    printBoard(Column_identifier, Column_list)
+
+def Hero_Movement(Column_identifier, Column_list, startrow): #This puts the above three functions in one
+    initial_location = [int(startrow),1]
+    print(initial_location, "row", initial_location[0], initial_location[1])
+    #Check adjacent rooms - check_adjacent(Column_identifier, Column_List, Current_row, Current_column)
+    #Returns ["Boss"/"next", row, column]
+    new_location = check_adjacent(Column_identifier, Column_list, initial_location[0], initial_location[1])
+    bossreached = False
+    while bossreached == False:
+        if new_location[0] == "Boss":
+            bossreached = True
+        else:
+            new_location = check_adjacent(Column_identifier, Column_list, new_location[1], new_location[2])
+        hero_location(Column_identifier, Column_list)
+    print("It worked.")
+
+Hero_Movement(Column_identifier, Column_list, startrow)
+print("Hero movement complete.")
